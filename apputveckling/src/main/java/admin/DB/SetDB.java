c72086d03b0821790986895770dd2529820738d1
JohannesJoujo <jagj2723@gmail.com>
2023-11-27 16:43:35 +0100

    inte helt klar med att fixa db:n

---
+++ apputveckling/src/main/java/admin/DB/SetDB.java
@@ -0,0 +1,14 @@
+package admin.DB;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+public class SetDB {
+    public MysqlDataSource setDB() {
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setURL("jdbc:mysql://localhost:3306/ProjectDB");
+        mysqlDataSource.setUser("root");
+        mysqlDataSource.setPassword("malek123");
+
+        return mysqlDataSource;
+    }
+}
