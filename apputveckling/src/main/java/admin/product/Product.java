200a072a6ee652840a9f1a8523f0bc51a37c03ff
Nasir Alizade <94725113+nasiralizade@users.noreply.github.com>
2023-12-02 16:19:06 +0100

    produkter hämtas från db nu

--- apputveckling/src/main/java/admin/product/Product.java
+++ apputveckling/src/main/java/admin/product/Product.java
@@ -1,110 +1,74 @@
 package admin.product;
 
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "PRODUCT")
 public class Product {
-    private int id;
-    List<String> images = List.of("BobMarley.JPG", "IMG_1186.JPG", "IMG_1187.JPG", "IMG_1188.JPG", "IMG_1189.JPG");
 
-    private String name;
-    private String description;
-    private String price;
-    private String quantity; //the number of items in stock
-    private String history;
-    private String category;
-    private String image;
 
-    public Product(int id, String name, String description, String price, String quantity, String category, String image) {
-
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.quantity = quantity;
-        this.category = category;
-        this.image = image;
-    }
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long PRODUCT_ID;
+    @Column(name = "PRODUCT_NAME")
+    private String PRODUCT_NAME;
+    @Column(name = "PRODUCT_PRICE")
+    private int PRODUCT_PRICE;
+    @Column(name = "PRODUCT_MAIN_DESC")
+    private String PRODUCT_MAIN_DESC;
+    @Column(name = "PRODUCT_HISTORY_DESC")
+    private String PRODUCT_HISTORY_DESC;
+    @Column(name = "PRODUCT_YEAR")
+    private int PRODUCT_YEAR;
 
     public Product() {
-        this.name = "";
-        this.description = "";
-        this.price = "";
-        this.quantity = "";
-        this.category = "";
-        this.image = "";
     }
 
-    public int getId() {
-        return id;
+    public Long getPRODUCT_ID() {
+        return PRODUCT_ID;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setPRODUCT_ID(Long PRODUCT_ID) {
+        this.PRODUCT_ID = PRODUCT_ID;
     }
 
-    public String getName() {
-        return name;
+    public String getPRODUCT_NAME() {
+        return PRODUCT_NAME;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPRODUCT_NAME(String PRODUCT_NAME) {
+        this.PRODUCT_NAME = PRODUCT_NAME;
     }
 
-    public String getDescription() {
-        return description;
+    public int getPRODUCT_PRICE() {
+        return PRODUCT_PRICE;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setPRODUCT_PRICE(int PRODUCT_PRICE) {
+        this.PRODUCT_PRICE = PRODUCT_PRICE;
     }
 
-    public String getPrice() {
-        return price;
+    public String getPRODUCT_MAIN_DESC() {
+        return PRODUCT_MAIN_DESC;
     }
 
-    public void setPrice(String price) {
-        this.price = price;
+    public void setPRODUCT_MAIN_DESC(String PRODUCT_MAIN_DESC) {
+        this.PRODUCT_MAIN_DESC = PRODUCT_MAIN_DESC;
     }
 
-    public String getQuantity() {
-        return quantity;
+    public String getPRODUCT_HISTORY_DESC() {
+        return PRODUCT_HISTORY_DESC;
     }
 
-    public void setQuantity(String quantity) {
-        this.quantity = quantity;
+    public void setPRODUCT_HISTORY_DESC(String PRODUCT_HISTORY_DESC) {
+        this.PRODUCT_HISTORY_DESC = PRODUCT_HISTORY_DESC;
     }
 
-    public String getHistory() {
-        return history;
+    public int getPRODUCT_YEAR() {
+        return PRODUCT_YEAR;
     }
 
-    public void setHistory(String history) {
-        this.history = history;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public List<String> getImages() {
-        return images;
-    }
-
-    public void setImages(List<String> images) {
-        this.images = images;
+    public void setPRODUCT_YEAR(int PRODUCT_YEAR) {
+        this.PRODUCT_YEAR = PRODUCT_YEAR;
     }
 }
---
+++ apputveckling/src/main/java/admin/product/ProductBean.java
@@ -0,0 +1,64 @@
+package admin.product;
+
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Named;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import javax.sql.DataSource;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+@Named
+@RequestScoped
+public class ProductBean implements Serializable {
+    @Produces
+    @PersistenceContext(unitName = "PRODUCT")
+    private EntityManager entityManager;
+    List<Product> products;
+    List<Product> productsDetails;
+    private String isShowProductDetails = "false";
+
+    public String getIsShowProductDetails() {
+        return isShowProductDetails;
+    }
+
+    public void setIsShowProductDetails(String isShowProductDetails) {
+        this.isShowProductDetails = isShowProductDetails;
+    }
+
+    public ProductBean() {
+    }
+
+
+
+    public void setProductsDetails(List<Product> productsDetails) {
+        this.productsDetails = productsDetails;
+    }
+
+    public void showProductDetails(int productId){
+        productsDetails = entityManager.createQuery("select p from Product p where p.PRODUCT_ID = :productId", Product.class)
+                .setParameter("productId", productId)
+                .getResultList();
+        isShowProductDetails = "true";
+
+    }
+
+    public List<Product> getProducts() {
+        products = entityManager.createQuery("select p from Product p", Product.class).getResultList();
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> getProductsDetails() {
+        return productsDetails;
+    }
+}
