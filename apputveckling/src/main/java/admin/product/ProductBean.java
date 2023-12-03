33609e4f9230d38a1f2fc565611d705c690ba511
Nasir Alizade <94725113+nasiralizade@users.noreply.github.com>
2023-12-03 17:58:12 +0100

    deleteProduct och addProduct fixat

--- apputveckling/src/main/java/admin/product/ProductBean.java
+++ apputveckling/src/main/java/admin/product/ProductBean.java
@@ -85,5 +85,36 @@ public class ProductBean implements Serializable {
         return productsDetails;
     }
 
+    /**
+     * this method is used to add a product to the database
+     * @param productName the name of the product
+     * @param productPrice the price of the product
+     * @param productMainDesc the main description of the product
+     * @param productHistoryDesc the history description of the product
+     * @param productYear the year of the product
+     */
+    public void addProduct(String productName, int productPrice, String productMainDesc, String productHistoryDesc, int productYear){
+        Product product = new Product();
+        product.setPRODUCT_NAME(productName);
+        product.setPRODUCT_PRICE(productPrice);
+        product.setPRODUCT_MAIN_DESC(productMainDesc);
+        product.setPRODUCT_HISTORY_DESC(productHistoryDesc);
+        product.setPRODUCT_YEAR(productYear);
+        entityManager.persist(product);
+    }
+
+    /**
+     * this method is used to update a product in the database
+     * @param productId the id of the product
+     */
+
+    public void deleteProduct(int productId){
+        entityManager.createQuery("delete from Product p where p.PRODUCT_ID = :productId")
+                .setParameter("productId", productId)
+                .executeUpdate();
+       ProductImagesBean productImagesBean = new ProductImagesBean();
+         productImagesBean.deleteProductImages(productId);
+    }
+
 
 }
