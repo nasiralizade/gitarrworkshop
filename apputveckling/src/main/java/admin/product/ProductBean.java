579d20a4e845350ef64b65b608c9b4d9ff98d120
Nasir Alizade <94725113+nasiralizade@users.noreply.github.com>
2023-12-03 23:23:21 +0100

    addProduct funkar från websidan, TODO: att hantera bilden/bilder kvar!

--- apputveckling/src/main/java/admin/product/ProductBean.java
+++ apputveckling/src/main/java/admin/product/ProductBean.java
@@ -4,7 +4,10 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.PersistenceContext;
+import jakarta.servlet.http.Part;
+import jakarta.transaction.Transactional;
 
 import java.io.Serializable;
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.List;
 /**
  * this class is used to get the list of products from the database
  * and to show the details of a specific product
+ *
  * @see Product for the product entity
  */
 @Named
@@ -23,9 +27,21 @@ public class ProductBean implements Serializable {
     List<Product> products; // used to get the list of products from the database
     List<Product> productsDetails; // used to show the details of a specific product
     private String isShowProductDetails = "false"; // used to show the details of a specific product
+    Product newProduct = new Product(); // used to add a product to the database
+    List<ProductImages> productImagesList; // used to get the list of product images from the database
+    private Part saveProductImages; // used to save the product images to the database
+
+    public Product getNewProduct() {
+        return newProduct;
+    }
+
+    public void setNewProduct(Product newProduct) {
+        this.newProduct = newProduct;
+    }
 
     /**
      * this method is used to get the value of isShowProductDetails
+     *
      * @return the value of isShowProductDetails
      */
     public String getIsShowProductDetails() {
@@ -40,19 +56,19 @@ public class ProductBean implements Serializable {
     }
 
 
-
     public void setProductsDetails(List<Product> productsDetails) {
         this.productsDetails = productsDetails;
     }
 
     /**
      * this method is used to show the details of a specific product
-     * @see Product for the product entity
+     *
      * @param productId the id of the product
      *                  the id is used to get the product from the database
      *                  and to show the details of the product
+     * @see Product for the product entity
      */
-    public void showProductDetails(int productId){
+    public void showProductDetails(int productId) {
         productsDetails = entityManager.createQuery("select p from Product p where p.PRODUCT_ID = :productId", Product.class)
                 .setParameter("productId", productId)
                 .getResultList();
@@ -62,6 +78,7 @@ public class ProductBean implements Serializable {
 
     /**
      * this method is used to get the list of products from the database
+     *
      * @return the list of products
      */
     public List<Product> getProducts() {
@@ -71,6 +88,7 @@ public class ProductBean implements Serializable {
 
     /**
      * this method is used to set the list of products
+     *
      * @param products the list of products
      */
     public void setProducts(List<Product> products) {
@@ -79,6 +97,7 @@ public class ProductBean implements Serializable {
 
     /**
      * this method is used to get the list of products from the database
+     *
      * @return the list of products
      */
     public List<Product> getProductsDetails() {
@@ -86,34 +105,26 @@ public class ProductBean implements Serializable {
     }
 
     /**
-     * this method is used to add a product to the database
-     * @param productName the name of the product
-     * @param productPrice the price of the product
-     * @param productMainDesc the main description of the product
-     * @param productHistoryDesc the history description of the product
-     * @param productYear the year of the product
+     * this method is used to get the list of product images from the database
+     * TODO: add the product images to the database
      */
-    public void addProduct(String productName, int productPrice, String productMainDesc, String productHistoryDesc, int productYear){
-        Product product = new Product();
-        product.setPRODUCT_NAME(productName);
-        product.setPRODUCT_PRICE(productPrice);
-        product.setPRODUCT_MAIN_DESC(productMainDesc);
-        product.setPRODUCT_HISTORY_DESC(productHistoryDesc);
-        product.setPRODUCT_YEAR(productYear);
-        entityManager.persist(product);
+    @Transactional
+    public void addProduct() {
+        entityManager.persist(newProduct);
     }
+
 
     /**
      * this method is used to delete a product in the database
+     *
      * @param productId the id of the product
      */
-
-    public void deleteProduct(int productId){
+    public void deleteProduct(int productId) {
         entityManager.createQuery("delete from Product p where p.PRODUCT_ID = :productId")
                 .setParameter("productId", productId)
                 .executeUpdate();
-       ProductImagesBean productImagesBean = new ProductImagesBean();
-         productImagesBean.deleteProductImages(productId);
+        ProductImagesBean productImagesBean = new ProductImagesBean();
+        productImagesBean.deleteProductImages(productId);
     }
 
 
