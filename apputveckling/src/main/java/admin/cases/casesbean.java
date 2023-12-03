1b4fdb429b6856605fc26d7b0eab6d8f6d706193
BasharLevin <basharaboalneaj@outlook.com>
2023-12-03 14:33:43 +0100

    Jag har fixat "case" klassen och lagt till nya funktioner i "casesbean"

--- apputveckling/src/main/java/admin/cases/casesbean.java
+++ apputveckling/src/main/java/admin/cases/casesbean.java
@@ -1,49 +1,31 @@
 package admin.cases;
 
-public class casesbean {
-    int case_id;
-    String case_desc;
-    String case_status;
-    String case_start_date;
-    String case_end_date;
-    float case_hours;
-    public static void main(String[] args){
-        casesbean my_case = new casesbean();
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import jdk.jfr.Name;
+
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class casesbean implements Serializable {
+    private cases my_case;
+
+    public casesbean(){
+        my_case = new cases();
+
+        my_case.setCase_id(1);
+
+        my_case.setCase_desc("Khara");
     }
-    public String getCase_desc(){
-        return case_desc;
+
+    public cases getMy_case() {
+        return my_case;
     }
-    public String getCase_start_date(){
-        return case_start_date;
+
+    public void setMy_case(cases my_case) {
+        this.my_case = my_case;
     }
-    public String getCase_end_date(){
-        return case_end_date;
-    }
-    public String getCase_status(){
-        return case_status;
-    }
-    public int getCase_id(){
-        return case_id;
-    }
-    public float getCase_hours(){
-        return case_hours;
-    }
-    public void setCase_desc(String case_desc){
-        this.case_desc = case_desc;
-    }
-    public void setCase_status(String stat){
-        this.case_status = stat;
-    }
-    public void setCase_id(int caseId){
-        this.case_id = caseId;
-    }
-    public void setCase_start_date(String startDate){
-        this.case_start_date = startDate;
-    }
-    public void setCase_end_date(String endDate){
-        this.case_end_date = endDate;
-    }
-    public void setCase_hours(float hours){
-        this.case_hours = hours;
-    }
+
+
 }
