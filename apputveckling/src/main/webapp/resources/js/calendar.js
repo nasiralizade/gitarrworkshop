7e939837053650e20f4430c6089be1821f01e93b
local_tweekerz <94663534+LocalTweekez@users.noreply.github.com>
2023-12-03 21:05:01 +0100

    Added weekly + monthly + widget calendar

--- apputveckling/src/main/webapp/resources/js/calendar.js
+++ apputveckling/src/main/webapp/resources/js/calendar.js
@@ -1,11 +1,27 @@
-var calendarInstance1 = new calendarJs( "calendar", {
-    manualEditingEnabled: true
-    // All your options can be set here
+
+
+
+var calendarInstanceMonthly = new calendarJs( "calendarMonthly", {
+    manualEditingEnabled: false,
+    showExtraTitleBarButtons: false,
 } );
 
-// OR
-var calendarElement = document.getElementById( "calendar" );
-var calendarInstance2 = new calendarJs( calendarElement, {
-    manualEditingEnabled: true
-    // All your options can be set here
+var calendarInstanceWeekly = new calendarJs("calendarWeekly", {
+    manualEditingEnabled: false,
+    visibleDays: [0, 1, 2, 3, 4],
+    workingDays: [0, 1, 2, 3, 4],
+    viewToOpenOnFirstLoad: "full-week",
+    workingHoursStart: "10:00",
+    workingHoursEnd: "18:00",
+    showWeekendDays: false,
+    weekendDays: [5,6],
+    minutesBetweenSections: "60",
+    defaultEventDuration: 60,
+    useLocalStorageForEvents: true,
+} );
+
+
+var calendarInstanceWidget = new calendarJs( "calendarWidget", {
+    manualEditingEnabled: false,
+    isWidget: true,
 } );
