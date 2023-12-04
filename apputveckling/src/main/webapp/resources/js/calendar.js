eacd2586e7d9c457e52f7c4755ed3d01a995e610
LocalTweekez <serhad14@live.se>
2023-12-04 14:45:58 +0100

    Final touch for prototype2

--- apputveckling/src/main/webapp/resources/js/calendar.js
+++ apputveckling/src/main/webapp/resources/js/calendar.js
@@ -7,7 +7,7 @@ var calendarInstanceMonthly = new calendarJs( "calendarMonthly", {
 } );
 
 var calendarInstanceWeekly = new calendarJs("calendarWeekly", {
-    manualEditingEnabled: false,
+    manualEditingEnabled: true,
     visibleDays: [0, 1, 2, 3, 4],
     workingDays: [0, 1, 2, 3, 4],
     viewToOpenOnFirstLoad: "full-week",
@@ -22,6 +22,17 @@ var calendarInstanceWeekly = new calendarJs("calendarWeekly", {
 
 
 var calendarInstanceWidget = new calendarJs( "calendarWidget", {
-    manualEditingEnabled: false,
+    manualEditingEnabled: true,
     isWidget: true,
 } );
+
+function deleteHours() {
+    var hourParent = document.getElementsByClassName("hours")[1];
+    var size = hourParent.childNodes.length;
+
+    for (let i = size - 1; i >= 0; i--) {
+        if (i < 10 || i > 18) {
+            hourParent.removeChild(hourParent.childNodes[i]);
+        }
+    }
+}
