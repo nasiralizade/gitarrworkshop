47ccbf88bb5e6dd5ea500afb0149a3e5f260d3fe
dread117 <98956905+dread117@users.noreply.github.com>
2023-12-06 11:05:38 +0100

    Produktsidan är mestadels klar.

--- apputveckling/src/main/java/admin/product/ProductImagesBean.java
+++ apputveckling/src/main/java/admin/product/ProductImagesBean.java
@@ -56,6 +56,7 @@ public class ProductImagesBean implements Serializable {
                 .setParameter("productId", productId)
                 .getResultList();
         return oneImage.get(0).getImgPathString();
+
     }
 
 
