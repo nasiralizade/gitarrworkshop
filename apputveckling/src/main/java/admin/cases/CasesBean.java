d324aca03afc70de21a59c3765f4dd3822a735cd
BasharLevin <basharaboalneaj@outlook.com>
2023-12-06 10:17:35 +0100

    CasesJournalBean added and add new case function

--- apputveckling/src/main/java/admin/cases/CasesBean.java
+++ apputveckling/src/main/java/admin/cases/CasesBean.java
@@ -1,133 +1,209 @@
 package admin.cases;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.Resource;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.persistence.*;
+import jakarta.servlet.http.Part;
+import jakarta.transaction.Transactional;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Named;
-
-import javax.sql.DataSource;
 import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Named
-@ViewScoped
-public class CasesBean implements Serializable {
-    @Resource(name = "mysql_web")
-    private DataSource dataSource;
-    private static final Logger LOGGER = Logger.getLogger(CasesBean.class.getName());
-    private List<Cases> my_cases;
-    private Cases clientToEdit;
-    @PostConstruct
-    public void init() {
-        // Initialize clientToEdit
-        clientToEdit = new Cases();
-        // Load member data from the database during bean initialization
-        loadClients();
+@Transactional
+@SessionScoped
+public class CasesBean implements Serializable{
+    @Produces
+    @PersistenceContext(unitName = "test")
+    private EntityManager entityManager;
+    List<Cases> cases;
+    List<Cases> cases_details;
+    private String isShowProductDetails = "fales";
+    Cases newCase = new Cases();
+    private String newCaseDesc;
+    private String newCaseStatus;
+    private String newCaseDateStart;
+    private String newCaseDateEnd;
+    private String newCaseProfit;
+    private int newCaseHours;
+    private String newCaseType;
+
+    // Fields for adding a new journal
+    private String newJournalDesc;
+    private int newJournalId;
+
+
+    // Getter and setter methods for the new case fields
+    public String getNewCaseDesc() {
+        return newCaseDesc;
     }
 
+    public void setNewCaseDesc(String newCaseDesc) {
+        this.newCaseDesc = newCaseDesc;
+    }
 
-    private void loadClients() {
-        my_cases = new ArrayList<>();
+    public String getNewCaseStatus() {
+        return newCaseStatus;
+    }
 
-        try (Connection connection = dataSource.getConnection()) {
-            String sql = "SELECT * FROM CASES";
+    public void setNewCaseStatus(String newCaseStatus) {
+        this.newCaseStatus = newCaseStatus;
+    }
 
-            try (PreparedStatement statement = connection.prepareStatement(sql);
-                 ResultSet resultSet = statement.executeQuery()) {
+    public String getNewCaseDateStart() {
+        return newCaseDateStart;
+    }
 
-                while (resultSet.next()) {
-                    my_cases.add(mapResultSetToClient(resultSet));
-                }
-            }
-        } catch (SQLException e) {
-            Logger.getLogger(CasesBean.class.getName()).log(Level.SEVERE, "SQL Exception", e);
+    public void setNewCaseDateStart(String newCaseDateStart) {
+        this.newCaseDateStart = newCaseDateStart;
+    }
+
+    public String getNewCaseDateEnd() {
+        return newCaseDateEnd;
+    }
+
+    public void setNewCaseDateEnd(String newCaseDateEnd) {
+        this.newCaseDateEnd = newCaseDateEnd;
+    }
+
+    public String getNewCaseProfit() {
+        return newCaseProfit;
+    }
+
+    public void setNewCaseProfit(String newCaseProfit) {
+        this.newCaseProfit = newCaseProfit;
+    }
+
+    public int getNewCaseHours() {
+        return newCaseHours;
+    }
+
+    public void setNewCaseHours(int newCaseHours) {
+        this.newCaseHours = newCaseHours;
+    }
+
+    public String getNewCaseType() {
+        return newCaseType;
+    }
+
+    public void setNewCaseType(String newCaseType) {
+        this.newCaseType = newCaseType;
+    }
+
+    // Getter and setter methods for the new journal fields
+    public String getNewJournalDesc() {
+        return newJournalDesc;
+    }
+
+    public void setNewJournalDesc(String newJournalDesc) {
+        this.newJournalDesc = newJournalDesc;
+    }
+
+    public int getNewJournalId() {
+        return newJournalId;
+    }
+
+    public void setNewJournalId(int newJournalId) {
+        this.newJournalId = newJournalId;
+    }
+
+    public CasesBean(){}
+
+    public void setCasesDetails(List<Cases> cases_details){
+        this.cases_details = cases_details;
+    }
+
+    public void showProductDetails(int caseId){
+        cases_details = entityManager.createQuery("select p from Cases p where p.CASE_ID = :caseId", Cases.class)
+            .setParameter("caseId", caseId)
+                .getResultList();
+        isShowProductDetails = "true";
+    }
+
+    public List<Cases> getCases(){
+        cases = entityManager.createQuery("select p from Cases p", Cases.class).getResultList();
+        return cases;
+    }
+
+    public void setCases(List<Cases> cases){
+        this.cases = cases;
+    }
+
+    public void addCase() {
+        try {
+            // Create a new Cases instance
+            Cases newCase = new Cases();
+            newCase.setCASE_DESC(newCaseDesc);
+            newCase.setCASE_STATUS(newCaseStatus);
+            newCase.setCASE_DATE_START(newCaseDateStart);
+            newCase.setCASE_DATE_END(newCaseDateEnd);
+            newCase.setCASE_PROFIT(newCaseProfit);
+            newCase.setCASE_HOURS(newCaseHours);
+            newCase.setCASE_TYPE(newCaseType);
+
+            // Create a new CaseJournal instance
+            CaseJournal newJournal = new CaseJournal();
+            newJournal.setJOURNAL_DESC(newJournalDesc);
+            newJournal.setJOURNAL_ID(newJournalId);
+            newJournal.setaCase(newCase);
+
+            // Add the new CaseJournal to the list of case journals in the Cases object
+            newCase.getCaseJournals().add(newJournal);
+
+            // Persist the changes
+            entityManager.persist(newCase);
+
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Case added successfully!", null));
+
+            // Reset input fields for future additions
+            resetInputFields();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error adding Case: " + e.getMessage(), null));
         }
     }
-
-    // Encapsulate the logic for mapping a row in the result set to a Member object
-    private Cases mapResultSetToClient(ResultSet resultSet) throws SQLException {
-        Cases client = new Cases();
-        client.setCase_id(resultSet.getInt("CASE_ID"));
-        client.setCase_desc(resultSet.getString("CASE_DESC"));
-        client.setCase_status(resultSet.getString("CASE_STATUS"));
-        client.setCase_type(resultSet.getString("CASE_TYPE"));
-        return client;
-    }
-/*
-
-    public void editClient(int clientID) {
-        try (Connection connection = dataSource.getConnection()) {
-            String sql = "SELECT * FROM CASES WHERE CASE_ID = ?";
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                statement.setInt(1, clientID);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    if (resultSet.next()) {
-                        clientToEdit = mapResultSetToClient(resultSet);
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            Logger.getLogger(CasesBean.class.getName()).log(Level.SEVERE, "SQL Exception", e);
-        }
+    private void resetInputFields() {
+        newCaseDesc = null;
+        newCaseStatus = null;
+        newCaseDateStart = null;
+        newCaseDateEnd = null;
+        newCaseProfit = null;
+        newCaseHours = 0;
+        newCaseType = null;
+        newJournalDesc = null;
+        newJournalId = 0;
     }
 
-    public Cases getClientToEdit() {
-        return clientToEdit;
-    }
-    public String saveOrUpdateClient() {
-        try (Connection connection = dataSource.getConnection()) {
-            String sql;
-            if (clientToEdit.getCase_id() == -1) {
-                sql = "INSERT INTO CASES (CLIENT_NAME, CLIENT_PHONE, CLIENT_EMAIL, CLIENT_DATE) VALUES (?, ?, ?, CURRENT_DATE)";
-            } else {
-                sql = "UPDATE CLIENT SET CLIENT_NAME = ?, CLIENT_PHONE = ?, CLIENT_EMAIL= ? WHERE CLIENT_ID = ?";
-            }
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                statement.setString(2, clientToEdit.getCase_desc());
-                statement.setString(3, clientToEdit.getCase_status());
-                if (clientToEdit.getCase_id() != -1) {
-                    statement.setInt(4, clientToEdit.getCase_id());
-                }
-                statement.executeUpdate();
-            }
-        } catch (SQLException e) {
-            Logger.getLogger(CasesBean.class.getName()).log(Level.SEVERE, "SQL Exception", e);
-        }
-        loadClients();
-        return "cases"; // Return the name of the page where the user can see the member list
-    }
+    public static void main(String[] args) {
+        // This main method can be used for testing the addCase functionality
+        // Create an instance of CasesBean using a CDI container
+        CasesBean casesBean = new CasesBean();
 
-    public void addClient() {
-        clientToEdit = new Cases(); // Create a new Member object
-        clientToEdit.setCase_id(-1); // Set memberID to -1 to represent a new member
+        // Set values for the new case and journal (you may modify these as needed)
+        casesBean.setNewCaseDesc("Test Case");
+        casesBean.setNewCaseStatus("InProgress");
+        casesBean.setNewCaseDateStart("2023-01-01");
+        casesBean.setNewCaseDateEnd("2023-01-15");
+        casesBean.setNewCaseProfit("1000");
+        casesBean.setNewCaseHours(10);
+        casesBean.setNewCaseType("TypeA");
+
+        casesBean.setNewJournalDesc("Journal for Test Case");
+        casesBean.setNewJournalId(1);
+
+        // Call the addCase method
+        casesBean.addCase();
+    }
+    public String editCase(int caseId) {
+        return "/includes/editCase?faces-redirect=true&caseId=" + caseId;
     }
 
 
-    public String deleteEntity(int clientID) {
-        try (Connection connection = dataSource.getConnection()) {
-            String sql = "DELETE FROM CLIENT WHERE CLIENT_ID = ?";
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                statement.setInt(1, clientID);
-                statement.executeUpdate();
-            }
-        } catch (SQLException e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("An error occurred while deleting the member."));
-            Logger.getLogger(CasesBean.class.getName()).log(Level.SEVERE, "SQL Exception", e);
-        }
-        loadClients();
-        return "clients"; // Return the name of the page where the user can see the member list
-    }
-*/
-    // Getter method for the members list
-    public List<Cases> getMy_cases() {
-        return my_cases;
-    }
 }
